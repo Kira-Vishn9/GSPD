96bd6c1e5d8cf9dd4441d50dcee2b1f930bb97e7
Kira-Vishn9 <Kvishn9@gmail.com>
2023-09-06 17:27:34 -0400

    feat: and new page for looking posts

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import CssBaseline from '@mui/material/CssBaseline';
+import './App.css'
+import Main from "@/page/Main/Main.tsx";
+import { Box, deepOrange, amber, grey, type PaletteMode, ThemeProvider, createTheme} from "@muiDep/index.ts";
+import SignIn from "@/modules/SignIn/SignIn.tsx";
+import SignUp from "@modules/SignUp/SignUp.tsx";
+import {ColorModeContext} from "@context/ColorModeContext.ts";
+import {authContext} from "@context/AuthContext.ts";
+import Layout from "@/Layout/Layout.tsx";
+import Profile from "@/page/Profile/Profile.tsx";
+import OpenCard from "@/page/OpenCard/OpenCard.tsx";
+import FeedPosts from '@/page/FeedPosts/FeedPosts.tsx'
+import CreatePost from '@/page/CreatePost/CreatePost.tsx'
+
+const getDesignTokens = (mode: PaletteMode) => ({
+    palette: {
+        mode,
+        ...(mode === 'light'
+            ? {
+                primary: {
+                    main: "#1d1a1a",
+                },
+                divider: amber[200],
+                background: {
+                    default: '#ffffff',
+                },
+                text: {
+                    primary: grey[900],
+                    secondary: grey[900],
+                },
+
+            }
+            : {
+                primary: {
+                    main: '#fff'
+                },
+                divider: deepOrange[700],
+                background: {
+                    default: '#000000',
+                },
+                text: {
+                    primary: grey[100],
+                    secondary: grey[100],
+                },
+
+            }),
+    },
+});
+
+function App() {
+    const [auth, setAuth] = React.useState(true);
+    return (
+    <>
+        <authContext.Provider value={{ auth, setAuth }}>
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route path="*" element={<Main />} />
+                        <Route path={'/signin'} element={<SignIn />} />
+                        <Route path={'/signup'} element={<SignUp />} />
+                        <Route path={'/profile'} element={<Profile />} />
+                        <Route path={'/info'} element={<OpenCard />} />
+                        <Route path={'/home'} element={<FeedPosts />} />
+                        <Route path={'/create'} element={<CreatePost />} />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
+        </authContext.Provider>
+    </>
+  )
+}
+
+export default function ToggleColorMode() {
+    const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            },
+        }),
+        [],
+    );
+
+    const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+    const contextValue = {
+        mode: mode,
+        toggleColorMode: colorMode.toggleColorMode,
+    };
+
+    return (
+        <ColorModeContext.Provider value={contextValue}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Box
+                    sx={{
+                        m: '0px auto',
+                        bgcolor: 'background.default',
+                        color: 'text.primary',
+                        borderRadius: 1,
+                        p: 0
+                    }}
+                >
+                  <App />
+                </Box>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
+}
---
+++ react-ts/src/modules/SignIn/SignIn.tsx
@@ -0,0 +1,50 @@
+import styles from './style.module.css'
+import {Box, TextField, Button} from "@muiDep/index.ts";
+import * as Links from 'react-router-dom'
+import {SubmitHandler, useForm} from "react-hook-form";
+import React from "react";
+
+
+type Inputs ={
+    mail: string,
+    password: string,
+};
+const SignIn = () => {
+    const { register, handleSubmit} = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = async () => {
+     console.log('hi')
+    }
+
+    return(
+        <div className={styles.wrap}>
+            <h2>
+                Sign In
+            </h2>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch', mt: 3, display: 'flex' },
+                }}
+                noValidate
+                autoComplete="off"
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <TextField id="standard-basic" label="Email" variant="standard" {...register("mail", { required: true, pattern: /^\S+@\S+$/i })} />
+                <TextField id="standard-basic" label="Password" variant="standard" {...register("password")} />
+                <span className={styles.strSign}>
+                    <pre>
+                        If you haven't account_
+                        <Links.Link to="/signup">
+                              Sign Up
+                        </Links.Link>
+                    </pre>
+                </span>
+                    <Button type="submit" variant="outlined" color='primary'>
+                        Enter
+                    </Button>
+            </Box>
+        </div>
+    )
+}
+
+export default SignIn;
